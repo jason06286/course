f4ba0b003a2d8371c0df26da988c417b7036fced
jason06286 <dj4871114@gmail.com>
2020-06-11 20:21:32 +0800

    123

---
+++ week08/js/all.js
@@ -0,0 +1,55 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    // init: false,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    breakpoints: {
+        '@0.00': {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        '@0.75': {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        '@1.00': {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        '@1.50': {
+            slidesPerView: 4,
+            spaceBetween: 50,
+        },
+    }
+});
+
+var swiper2 = new Swiper('.swiper-container2', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    // init: false,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    breakpoints: {
+        '@0.00': {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        '@0.75': {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        '@1.00': {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        '@1.50': {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
+    }
+});
