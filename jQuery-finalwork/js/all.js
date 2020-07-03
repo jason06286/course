83152ba2391c6311fa5e91675ec6de555fe1e013
jason06286 <dj4871114@gmail.com>
2020-07-03 19:32:38 +0800

    123

--- jQuery-finalwork/js/all.js
+++ jQuery-finalwork/js/all.js
@@ -1,7 +1,27 @@
+//list slide
 $(document).ready(function () {
     $('.jq-list li a').click(function (e) { 
         e.preventDefault();
         $(this).parent().find('ul').slideToggle();
+    });
+});
+
+//ad-hide
+$(document).ready(function () {
+    $('.jq-ad').click(function (e) { 
+        e.preventDefault();
+        $('.jq-ad-none').hide();
+        
+    });
+});
+//top
+$(document).ready(function () {
+    $('.jq-top ').click(function (e) { 
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop:0
+        },1500);
+        
     });
 });
 //swiper
