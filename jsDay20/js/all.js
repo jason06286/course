b62004a9b09348f4d4b9e936e1b0555b6078a0e1
jason06286 <dj4871114@gmail.com>
2020-07-04 18:12:47 +0800

    123

--- jsDay20/js/all.js
+++ jsDay20/js/all.js
@@ -3,6 +3,11 @@ let weightDom=document.querySelector('.weight');
 let listDom=document.querySelector('.list');
 let btnDom=document.querySelector('.btn');
 let btnOutlineDom=document.querySelector('.btn-outline');
+let modalDom=document.querySelector('.modal');
+let modalTimes=document.querySelector('.modal-times');
+let modalBmi=document.querySelector('.modal-bmi');
+let clearDom=document.querySelector('.clear');
+let averageDom=document.querySelector('.average');
 let data=[];
 const BMIData = {
 	overThin: {
@@ -30,21 +35,12 @@ const BMIData = {
 		statusText: "重度肥胖"
 	}
 };
-function addData(status, bmi) {
-    data.push({
-        height:heightDom.value,
-        weight:weightDom.value,
-        bmi:bmi,
-        status:`${BMIData[status].statusText}`,
-        class:`${BMIData[status].class}`
-    })
-}
-
+//計算BMI
 function caculate() {
-    let height=heightDom.value
+    let height= heightDom.value
     let weight=weightDom.value
     let bmi=Math.floor(100*weight/Math.pow(height/100,2))/100
-    if (bmi>0){
+    if(bmi>0){
         if(bmi<18.5){
             addData('overThin',bmi)
         }else if(bmi<24){
@@ -60,16 +56,26 @@ function caculate() {
         }
     }else{
         alert('請輸入正確資料')
-    }
-    console.log(bmi);
-    console.log(addData('overThin',bmi));
+    }  
 }
-
-
+//把資料加入到data
+function addData(status,bmi) {
+    data.push(
+        {
+            height:heightDom.value,
+            weight:weightDom.value,
+            bmi:bmi,
+            status:`${BMIData[status].statusText}`,
+            class:`${BMIData[status].class}`
+        }
+    )
+    
+}
+//渲染畫面
 function render() {
-	let str = "";
-	data.forEach(function (item) {
-		str += `z
+    let str=''
+data.forEach(item => {
+    str += `
 		<li class="${item.class}">
             <h3>${item.status}</h3>
             <p>BMI <span>${item.bmi}</span></p>
@@ -77,11 +83,42 @@ function render() {
             <p>weight: <span>${item.weight}kg</span></p>
         </li>
 		`;
-	});
-	listDom.innerHTML = str;
+});
+listDom.innerHTML=str;   
 }
-btnDom.addEventListener('click',function(){
-    caculate();
-    addData();
-    render()
+// modal 渲染畫面
+function modalRender() {
+    let bmiTotal=0
+    let len=data.length
+    data.forEach(item => {
+        bmiTotal+=item.bmi; 
+    });
+    modalTimes.textContent=len;
+    modalBmi.textContent=bmiTotal/len;
+    
+}
+//確認監聽事件
+btnDom.addEventListener('click',function () {
+    caculate();;
+    render();
+    heightDom.value=[];
+    weightDom.value=[];
+})
+//全部清除監聽事件
+btnOutlineDom.addEventListener('click',function () {
+    data=[];
+    render();
+})
+//監聽開關modal
+averageDom.addEventListener('click',function(){
+    let classDom=document.createAttribute('class');
+    classDom.value='modal d-block';
+    modalDom.setAttributeNode(classDom);
+    modalRender();
+})
+
+clearDom.addEventListener('click',function(){
+    let classDom=document.createAttribute('class');
+    classDom.value='modal d-none';
+    modalDom.setAttributeNode(classDom); 
 })
