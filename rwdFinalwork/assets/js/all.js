90c53667f74719331f80dad6fead8d8783f7c27e
jason06286 <dj4871114@gmail.com>
2020-07-09 14:04:54 +0800

    123

---
+++ rwdFinalwork/assets/js/all.js
@@ -0,0 +1,9 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.ham').click(function (e) {
+    e.preventDefault();
+    $('.jq-list').toggleClass('active');
+  });
+});
+//# sourceMappingURL=all.js.map
